50fa3010c3539534605ea2c62d5f7f05e2535409
jab0073 <93546944+jab0073@users.noreply.github.com>
2022-03-13 21:12:45 -0500

    Add files via upload

--- src/edu/uah/cs321/Frontend/Application.java
+++ src/edu/uah/cs321/Frontend/Application.java
@@ -1,10 +1,16 @@
 package edu.uah.cs321.Frontend;
 
+import edu.uah.cs321.Backend.AuthSystem;
+import edu.uah.cs321.Backend.UserDatabase;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 /***
- * Project Name: GroupProject-InitalThoughts
+ * Project Name: GroupProject-InitialThoughts
  * File Name: Application
  * Description: 
  * @auth justinbushue
@@ -19,9 +25,25 @@ public class Application extends JFrame {
 
 	public Application(String title) {
 		super(title);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter()
+		{
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				try {
+					AuthSystem.close();
+					UserDatabase.close();
+					System.out.println("AuthSystem and UserDatabase have been closed...");
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+
+				e.getWindow().dispose();
+			}
+		});
 
 		setSize( 480, 480 );
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		contentPanel = new JPanel(new CardLayout());
 

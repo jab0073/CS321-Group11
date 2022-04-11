4bf20e395b222557747b532bda6b2263f3b22673
Shawn Burnham <ShawnB4AU@icloud.com>
2022-04-11 11:34:22 -0500

    Added a rating and review text box along with a save button to the MoviePage. Also made it so that the rating box, review box, and save button only show if the user is not a guest. Lastly I made the MoviePage user Friendlier.

--- Group11Project/src/edu/uah/cs321/Frontend/MoviePage.java
+++ Group11Project/src/edu/uah/cs321/Frontend/MoviePage.java
@@ -1,10 +1,12 @@
 package edu.uah.cs321.Frontend;
 
 import edu.uah.cs321.Backend.Movie;
+import edu.uah.cs321.Backend.Review;
 import edu.uah.cs321.Backend.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /***
@@ -15,42 +17,55 @@ import java.awt.event.ActionListener;
  */
 public class MoviePage extends JPanel {
 	private static Movie movie;
+	private static User u;
 
 	private static JLabel movieTitleLabel;
 	private static JLabel movieDirectorLabel;
 	private static JLabel movieWritersLabel;
-	private static JLabel movieYearLabel;
 	private static JLabel moviePlotLabel;
 	private static JLabel movieGenresLabel;
 	private static JLabel movieReleaseLabel;
 	private static JLabel movieActorsLabel;
 	private static JLabel movieRuntimesLabel;
+	private static JLabel ratingLabel;
+	private static JLabel reviewLabel;
 
 	private static JPanel contentPanel;
 	private static JPanel movieInfoPanel;
+	private static JPanel userReviewPanel;
+
+	private static JScrollPane reviewPane;
+
+	private static JTextField ratingTextField;
+	private static JTextArea reviewTextArea;
 
 	private static JButton addToFavoritesButton;
+	private static JButton saveReviewButton;
 
 
 	public MoviePage(Movie movie) {
 		super();
 		this.movie = movie;
+		u=AccountPage.getUser();
 
 		contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel,BoxLayout.Y_AXIS));
 		movieInfoPanel = new JPanel();
-		movieInfoPanel.setMinimumSize(new Dimension(300,1000));
-		movieInfoPanel.setMaximumSize(new Dimension(300,1000));
 		movieInfoPanel.setLayout(new BoxLayout(movieInfoPanel,BoxLayout.Y_AXIS));
+		userReviewPanel = new JPanel();
+		userReviewPanel.setLayout(new BoxLayout(userReviewPanel,BoxLayout.Y_AXIS));
 
-		movieTitleLabel = new JLabel(this.movie.getTitle());
-		movieDirectorLabel = new JLabel(this.movie.getDirector());
-		movieWritersLabel = new JLabel(this.movie.getWriter());
-		movieYearLabel = new JLabel(String.valueOf(this.movie.getYear()));
-		moviePlotLabel = new JLabel(this.movie.getPlot());
-		movieGenresLabel = new JLabel(this.movie.getGenre());
-		movieReleaseLabel = new JLabel(this.movie.getReleased());
-		movieActorsLabel = new JLabel(String.join(", ", this.movie.getActors()));
-		movieRuntimesLabel = new JLabel(this.movie.getRuntime());
+		movieTitleLabel = new JLabel("Title:  " + this.movie.getTitle());
+		movieDirectorLabel = new JLabel("Director:  " + this.movie.getDirector());
+		movieWritersLabel = new JLabel("Writers:  " + this.movie.getWriter());
+		moviePlotLabel = new JLabel("Plot:  " + this.movie.getPlot());
+		movieGenresLabel = new JLabel("Genre:  " + this.movie.getGenre());
+		movieReleaseLabel = new JLabel("Release Date:  " + this.movie.getReleased());
+		movieActorsLabel = new JLabel("Actors:  " + String.join(", ", this.movie.getActors()));
+		movieRuntimesLabel = new JLabel("Runtime:  " + this.movie.getRuntime());
+		ratingLabel = new JLabel("Rating");
+		reviewLabel = new JLabel("Review");
+
 
 		addToFavoritesButton = new JButton("Favorite");
 		addToFavoritesButton.addActionListener(a -> {
@@ -60,18 +75,49 @@ public class MoviePage extends JPanel {
 			Application.setAccountPage(ac);
 		});
 
-		movieInfoPanel.add(movieTitleLabel, Component.CENTER_ALIGNMENT);
-		movieInfoPanel.add(Box.createVerticalGlue());
-		movieInfoPanel.add(movieActorsLabel, Component.CENTER_ALIGNMENT);
-		movieInfoPanel.add(Box.createVerticalGlue());
-		movieInfoPanel.add(movieDirectorLabel,Component.CENTER_ALIGNMENT);
-		movieInfoPanel.add(Box.createVerticalGlue());
-		movieInfoPanel.add(movieYearLabel, Component.CENTER_ALIGNMENT);
-		movieInfoPanel.add(Box.createVerticalGlue());
-		movieInfoPanel.add(movieGenresLabel,Component.CENTER_ALIGNMENT);
+		//im unsure how to make this less wide.
+		ratingTextField = new JTextField(5);
+		reviewTextArea = new JTextArea(10,25);
+		reviewPane = new JScrollPane(reviewTextArea);
+		reviewTextArea.setLineWrap(true);
 
+		saveReviewButton = new JButton("Save");
+		saveReviewButton.addActionListener(e -> {
+			//unfinished
+		});
+
+		movieInfoPanel.add(movieTitleLabel, Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieActorsLabel, Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieDirectorLabel,Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieWritersLabel,Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieGenresLabel,Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieReleaseLabel,Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		movieInfoPanel.add(movieRuntimesLabel,Component.CENTER_ALIGNMENT);
+		movieInfoPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+		//Plot Label shifts everything to the right and I can't figure out how to fix it
+		//movieInfoPanel.add(moviePlotLabel,Component.CENTER_ALIGNMENT);
+		//userReviewPanel.add(new Box.Filler(new Dimension(0,5),new Dimension(0,5),new Dimension(0,5)));
+
+		userReviewPanel.add(addToFavoritesButton, Component.CENTER_ALIGNMENT);
+		userReviewPanel.add(new Box.Filler(new Dimension(0,20),new Dimension(0,20),new Dimension(0,20)));
+		userReviewPanel.add(ratingLabel,Component.CENTER_ALIGNMENT);
+		userReviewPanel.add(ratingTextField, Component.CENTER_ALIGNMENT);
+		userReviewPanel.add(new Box.Filler(new Dimension(0,10),new Dimension(0,	10),new Dimension(0,10)));
+		userReviewPanel.add(reviewLabel,Component.CENTER_ALIGNMENT);
+		userReviewPanel.add(reviewPane, Component.CENTER_ALIGNMENT);
+		userReviewPanel.add(saveReviewButton, Component.CENTER_ALIGNMENT);
+
+		contentPanel.add(new Box.Filler(new Dimension(0,10),new Dimension(0,10),new Dimension(0,10)));
 		contentPanel.add(movieInfoPanel, Component.CENTER_ALIGNMENT);
-		contentPanel.add(addToFavoritesButton, Component.CENTER_ALIGNMENT);
+		if (u.getEntitlementType()){
+			contentPanel.add(userReviewPanel, Component.CENTER_ALIGNMENT);
+		}
 		add(contentPanel,Component.CENTER_ALIGNMENT);
 	}
 }
